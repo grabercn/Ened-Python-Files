1982dd633429649463e9035759233c6c37fad7ed
Christian Graber <cgrab713@gmail.com>
2021-11-10 17:23:34 -0500

    add back

---
+++ Python/HW_11p1_Task1_grabercn.py
@@ -0,0 +1,9 @@
+from typing import Hashable
+
+
+T = int(input('Enter outside Temp in F (-10-125): '))
+Wc = int(input('Enter Weather Conditions (3: sunny, 2: partly cloudy 0: cloudy): '))
+H = int(input('Enter Relative Humidity (0-1): '))
+L = int(input('Enter Number of Ladders on Construction Site: '))
+Hstruc = int(input('Enter Height of Structure in ft (20-2800): '))
+Sdry = int(input('Enter Surface Dryness (3: all surface wet, 2: puddles w some dry area, 0: completely dry): '))
