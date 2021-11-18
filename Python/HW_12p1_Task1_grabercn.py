1f94187729303566bf3ff86c3907b30ffc94b1a0
Christian Graber <cgrab713@gmail.com>
2021-11-17 20:31:45 -0500

    finish task 1 hw 12

--- Python/HW_12p1_Task1_grabercn.py
+++ Python/HW_12p1_Task1_grabercn.py
@@ -1,12 +1,25 @@
 import math
 
-while True:
+y=0
+g=9.81
+
+while y<2:
     V0 = int(input('Enter Initial Velocity (m/s): '))
     theta = int(input('Enter Desired Angle Increment (deg): '))
     
-    if not(0>theta>90):
-        break
-    if not(V0<=0):
-        break
-        
+    if not(theta>90 or theta<0):
+        y+=1
+    if not(V0 < 0):
+        y+=1
 
+i = 1
+while theta<90:
+    theta=22*i
+    Mh = ((V0**2)*(math.sin(math.radians(theta))**2)/(2*g))
+    It = ((2*V0)*math.sin(math.radians(theta))/(g))
+    
+    print('For Angle = {0:.2f} deg., Impact Time = {1:.2f} s and Max Height = {2:.2f} m'.format(theta, It, Mh))
+    i+=1
+    theta=22*i
+    
+    
